a266ab735ae071b32d6a4cf955cfebf49ae1715c
Glibert <thomasglibert@hotmail.com>
2018-02-18 13:51:59 +0100

    tous les fichiers à la fois

--- pages/Accueil.php
+++ pages/Accueil.php
@@ -1,14 +1,20 @@
-			<div>
-				<div>
-					
-				</div>
-			</div>	
-			<section>
-				<article>
-					
-				</article>
-				<aside>
-					
-				</aside>
-			</section>
-			
+<h2 id=titre_page">Entrées et sorties des marchandises</h2>
+<div id="image_accueil">
+	<section>
+		<h2>Achats des produits de décoration</h2>
+		<p>Achat de 1276 pièces au prix moyen de 17,35 euros</p>
+		<p>Vente de 834 pièces au prix moyen de 38,13 euros</p>
+	</section>
+	
+	<section>
+		<h2>Ventes des produits d'ameublement</h2>
+		<p>Achat de 816 pièces au prix moyen de 83,45 euros</p>
+		<p>Vente de 609 pièces au prix moyen de 138,71 euros</p>
+	</section>
+	
+	<section>
+		<h2>Contact Responsable des stocks</h2>
+		<p>Juliette : extension 18</p>
+	</section>
+
+</div>
